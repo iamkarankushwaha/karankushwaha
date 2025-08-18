403423db8d1c39444a3ba0013204bc9d3ca7cc8c
Karan Kushwaha <110368549+iamkarankushwaha@users.noreply.github.com>
2025-08-18 22:13:17 +0530

    update

--- JavaScripts/Featured-Products.js
+++ JavaScripts/Featured-Products.js
@@ -4,9 +4,18 @@ document.addEventListener("DOMContentLoaded", function () {
     // Loop through database and create cards
     itemsDatabase.forEach(item => {
         let card = document.createElement("div");
-        card.classList.add("prodbox");
         card.innerHTML = `
-        <div class="prodcase" onclick="prodorder(${item.id})">
+        <div class="card shadow rounded-3" style="width: 100%; max-width: 18rem; cursor: pointer;" onclick="prodorder(${item.id})">
+            <img src="${item.imgSrc}" class="card-img-top rounded-top-3" alt="${item.title}">
+            <div class="card-body">
+                <h6 class="card-title fs-6 fw-medium" style="text-align: justify; display: -webkit-box; -webkit-line-clamp: 3; -webkit-box-orient: vertical; overflow: hidden; text-overflow: ellipsis;">${item.title}</h6>
+                <span class="fs-5 fw-bold text-danger">${item.priceActive}</span>
+                <span class="fs-6 fw-bold text-muted px-2 text-decoration-line-through">${item.priceNonActive}</span>
+                <a href="#" class="btn btn-primary mt-3 py-2 fw-bold">Download<i class="bi bi-download px-2"></i></a>
+            </div>
+        </div>
+
+        <!--<div class="prodcase" onclick="prodorder(${item.id})">
             <img src="${item.imgSrc}" class="prodimg" alt="${item.title}">
             <div>
                 <p class="prod_title">${item.title}</p>
@@ -15,7 +24,7 @@ document.addEventListener("DOMContentLoaded", function () {
                 <h5 id="prodprice01">${item.priceActive}</h5> <span>${item.priceNonActive}</span>
             </div>
         </div>
-        <button id="prodbutton" class="ctabtns" onclick="prodorder(${item.id})">Download <span class="fa-solid fa-download"></span></button>
+        <button id="prodbutton" class="ctabtns" onclick="prodorder(${item.id})">Download <span class="fa-solid fa-download"></span></button>-->
         `;
         container.appendChild(card);
     });
