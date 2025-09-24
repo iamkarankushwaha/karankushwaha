2d05e16007b97b40e42f622e45e8be08a98269cf
Karan Kushwaha <110368549+iamkarankushwaha@users.noreply.github.com>
2025-09-24 11:33:51 +0530

    v2

--- service-worker.js
+++ service-worker.js
@@ -1,21 +1,33 @@
-self.addEventListener("install", event => {
-  event.waitUntil(
-    caches.open("my-site-cache").then(cache => {
-      return cache.addAll([
-        "/",          // homepage
-        "/index.html",
-        "/styles.css",
-        "/script.js",
-        "/Assets-Image/karan kushwaha-brand-logo.svg",  // preload your logo/icons
-      ]);
-    })
-  );
-});
+// self.addEventListener("install", event => {
+//   event.waitUntil(
+//     caches.open("my-site-cache").then(cache => {
+//       return cache.addAll([
+//         "/",
+//         "index.html",
+//         "styles/style.css",
+//         "styles/responsive-style.css",
+//         "Scripts/script.js",
+//         "Scripts/contact.js",
+//         "Assets-Image/karan kushwaha-brand-logo.svg",
+//       ]);
+//     })
+//   );
+// });
 
-self.addEventListener("fetch", event => {
-  event.respondWith(
-    caches.match(event.request).then(response => {
-      return response || fetch(event.request);
-    })
+// self.addEventListener("fetch", event => {
+//   event.respondWith(
+//     caches.match(event.request).then(response => {
+//       return response || fetch(event.request);
+//     })
+//   );
+// });
+
+
+
+self.addEventListener("activate", event => {
+  event.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(keys.map(key => caches.delete(key)))
+    )
   );
 });
